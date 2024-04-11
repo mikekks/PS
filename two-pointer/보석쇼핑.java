20f96363ab6b5262694fdbbec6a5898616bd4986
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-04-12 02:29:00 +0900

    Update 보석쇼핑.java

--- two-pointer/보석쇼핑.java
+++ two-pointer/보석쇼핑.java
@@ -3,27 +3,31 @@ import java.util.*;
 class Solution {    
     public int[] solution(String[] gems) {
         int kind = new HashSet<>(Arrays.asList(gems)).size();
- 
+        HashMap<String, Integer> map = new HashMap<>();
         int[] answer = new int[2];
-        int ans = 100005;
-        int start = 0;
-               
-        Map<String, Integer> map = new HashMap<>();
         
-        for (int end = 0; end < gems.length; end++) {
-            map.put(gems[end], map.getOrDefault(gems[end], 0) + 1);
- 
-            while (map.get(gems[start]) > 1) {
-                map.put(gems[start], map.get(gems[start]) - 1);
-                start++;
+        int left = 0;
+        int ans = 100005;
+        int cur_cnt = 0;
+        
+        for(int right=0; right < gems.length; right++){
+            
+            map.put(gems[right], map.getOrDefault(gems[right], 0) + 1);
+            
+            while(map.get(gems[left]) > 1){
+                map.put(gems[left], map.get(gems[left]) - 1);
+                left++;
             }
- 
-            if (map.size() == kind && ans > (end - start)) {
-                ans = end - start;
-                answer[0] = start + 1;
-                answer[1] = end + 1;
+            
+            if(right-left < ans && map.size() == kind){
+                ans = right-left;
+                answer[0] = left + 1;
+                answer[1] = right + 1;
             }
+            
         }
+        
+        
  
         return answer;
     }
