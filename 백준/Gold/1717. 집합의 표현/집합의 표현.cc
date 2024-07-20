9b875cf02236356d3a3a2c60ec3b3a268b81313f
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-07-21 00:37:07 +0900

    [Gold V] Title: 집합의 표현, Time: 32 ms, Memory: 6320 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,25 +1,36 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <queue>
+
+#define MAX 1000005
 
 using namespace std;
-int n, m;
-int parent[1000001];
 
-int getParent(int x) {
-    if (parent[x] == x) return x;
-    return parent[x] = getParent(parent[x]);
+// 13:10 시작
+
+int N, M;
+int p[MAX];
+int ans = 0;
+
+int find_parent(int a){
+    if(p[a] == a) return a;
+    
+    return p[a] = find_parent(p[a]);
 }
-void unionParent(int a, int b) {
-    a = getParent(a);
-    b = getParent(b);
-    if (a > b) parent[a] = b;
-    else parent[b] = a;
-}
-void findParent(int a, int b) {
-    a = getParent(a);
-    b = getParent(b);
-    if (a == b) cout << "YES\n";
-    else cout << "NO\n";
+
+void mergeNode(int a, int b){
+    a = find_parent(a);
+    b = find_parent(b);
+    
+    if(a < b){
+        p[b] = a;
+    }
+    else{
+        p[a] = b;
+    }
 }
 
 
@@ -27,22 +38,30 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
-        parent[i] = i;
+    
+    cin >> N >> M;
+    
+    for(int i=1; i<=N; i++){
+        p[i] = i;
     }
-    for (int i = 0; i < m; i++) {
-        int o, a, b;
-        cin >> o >> a >> b;
-        if (!o) {// union연산
-            unionParent(a, b);
+    
+    for(int i=0; i<M; i++){
+        int oper,a,b;
+        cin >> oper >> a >> b;
+        
+        if(oper == 0){
+            mergeNode(a,b);
         }
-        else { //find연산
-            findParent(a, b);
+        else{
+            int pA = find_parent(a);
+            int pB = find_parent(b);
+            if(pA == pB){
+                cout << "YES" << '\n';
+            }
+            else{
+                cout << "NO" << '\n';
+            }
         }
-
     }
-
-    return 0;
+    
 }
