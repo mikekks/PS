8b9dec2f051402d6fe6c9c15136461158f65a525
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-08-03 01:01:24 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.cc
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+
+#define MAX 105
+
+using namespace std;
+
+long long ans = 0;
+int N, T;
+int sy, sx;
+int ey, ex;
+vector<pair<int, int>> store;
+bool isVisit[MAX];  // 해당 편의점을 들리고는 절대 도착할 수 없음을 의미
+
+bool dfs(int y, int x){
+    
+    if(y == ey && x == ex){
+        cout << "happy" << '\n';
+        return true;
+    }
+    
+    if((ey-y + ex-x) <= 1000){
+        cout << "happy" << '\n';
+        return true;
+    }
+    
+    bool ret = false;
+    
+    
+    for(int i=0; i<store.size(); i++){
+        if(isVisit[i] == false){
+                int d = (store[i].first - y) + (store[i].second - x);
+                if(d <= 1000){
+                    isVisit[i] = true;
+                    ret = dfs(store[i].first, store[i].second);
+                    isVisit[i] = false;
+                    if(ret == false){
+                        isVisit[i] = true;
+                    }
+                    else{
+                        return true;
+                    }
+                }
+        }
+    }
+    
+    return ret;
+}
+
+int main(void)
+{
+    cin >> T;
+    
+    while (T--) {
+        store.clear();
+        memset(isVisit, '\0', sizeof(isVisit));
+        
+        cin >> N;
+        cin >> sy >> sx;
+        
+        for(int i=0; i<N; i++){
+            int ty, tx;
+            cin >> ty >> tx;
+            store.push_back({ty, tx});
+        }
+        
+        cin >> ey >> ex;
+        
+        queue<pair<int, int>> q;
+        q.push({sy, sx});
+        
+        bool ret = false;
+        
+        while (!q.empty()) {
+            int cy = q.front().first;
+            int cx = q.front().second;
+            q.pop();
+            
+            if(abs(ey-cy) + abs(ex-cx) <= 1000){
+                ret = true;
+                break;
+            }
+            
+            for(int i=0; i<store.size(); i++){
+                if(isVisit[i] == false){
+                        int d = abs(store[i].first - cy) + abs(store[i].second - cx);
+                        if(d <= 1000){
+                            isVisit[i] = true;
+                            q.push({store[i].first, store[i].second});
+                        }
+                }
+            }
+        }
+        
+        if(ret){
+            cout << "happy" << '\n';
+        }
+        else{
+            cout << "sad" << '\n';
+        }
+        
+        
+    }
+
+    return 0;
+}
