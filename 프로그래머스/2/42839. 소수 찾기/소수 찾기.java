39b525dda7078e02730b0ce54d6fabbeaeb34c7c
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-09-05 15:09:13 +0900

    [level 2] Title: 소수 찾기, Time: 36.82 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,50 +1,35 @@
+import java.util.*;
+
 class Solution {
     
-    boolean[] isVisit = new boolean[10];
-    boolean[] num = new boolean[10000000];
+    HashSet<Integer> set = new HashSet<>();
     
     public boolean isPos(int n){
         if(n == 0 || n == 1) return false;
         
-        for(int i=2; i<n; i++){
+        for(int i=2; i<=Math.sqrt(n); i++){
             if(n % i == 0) return false;
         }
         
         return true;
     }
     
-    public void dfs(String numbers, String cur, int d){
-        if(d >numbers.length()){
-            return;
-        }
+    public void dfs(String numbers, String cur){
+        int n = numbers.length();
         
-        
-        for(int i=0; i<numbers.length(); i++){
-            if(!isVisit[i]){
-                isVisit[i] = true;
-                num[Integer.parseInt(cur + numbers.charAt(i))] = true;
-                dfs(numbers, cur + numbers.charAt(i), d+1);
-                isVisit[i] = false;
-            }
+        for(int i=0; i<n; i++){
+            set.add(Integer.parseInt(cur + numbers.charAt(i)));
+            dfs(numbers.substring(0,i) + numbers.substring(i+1, n), cur + numbers.charAt(i));
         }
     }
     
     public int solution(String numbers) {
         int answer = 0;
         
-        for(int i=0; i<numbers.length(); i++){
-            if(numbers.charAt(i) != '0'){
-                isVisit[i] = true;
-                num[numbers.charAt(i) - '0'] = true;
-                dfs(numbers, String.valueOf(numbers.charAt(i)), 1);
-                isVisit[i] = false;
-            }
-        }
+        dfs(numbers, "");
         
-        for(int i=1; i<num.length; i++){
-            if(num[i]){
-                if(isPos(i)) answer++;
-            }
+        for(Integer i : set){
+            if(isPos(i)) answer++;
         }
         
         return answer;
