48afd44d40f5851f9d7328ee2f98be816057a90c
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-09-24 02:01:59 +0900

    [level 3] Title: 여행경로, Time: 84.62 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,31 +1,22 @@
 import java.util.*;
 
 class Solution {
-    
-    List<String> answers = new ArrayList<>();
-    boolean visit[] = new boolean[10005];
-    
-    public void dfs(String start, String route, String[][] tickets, int cnt){
-        if(cnt == tickets.length){
-            answers.add(route);
-        }
-        
-        for(int i=0; i<tickets.length; i++){
-            if(!visit[i] && tickets[i][0].equals(start)){
-                visit[i] = true;
-                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, cnt+1);
-                visit[i] = false;
-            }
-        }
-    }
-    
     public String[] solution(String[][] tickets) {
         String[] answer = {};
+        Map<String, List<String>> map = new HashMap<>();
         
-        dfs("ICN", "ICN", tickets, 0);
-        
-        Collections.sort(answers);
-        answer = answers.get(0).split(" ");
+        for(int i=0; i<tickets.length; i++){
+            if(!map.containsKey(tickets[i][0])){
+                List<String> tmp = new ArrayList<>();
+                tmp.add(tickets[i][1]);
+                map.put(tickets[i][0], tmp);
+            }
+            else{
+                List<String> tmp = map.get(tickets[i][0]);
+                tmp.add(tickets[i][1]);
+                map.put(tickets[i][0], tmp);
+            }
+        }
         
         return answer;
     }
