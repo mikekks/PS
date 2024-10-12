fda42c80aec81db2ac8a964283e8ee7bf44b4402
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-10-12 17:17:19 +0900

    [level 3] Title: 숫자 게임, Time: 63.30 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -4,17 +4,25 @@ class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
         
+        // A팀은 빠르게 출전순서를 정했고 자신들의 출전 순서를 B팀에게 공개
+        // A의 순서가 의미가 없다.
+        // 투포인터 - 12m
+        
         Arrays.sort(A);
         Arrays.sort(B);
         
         int aIdx = 0;
-        for(int i=0; i<B.length; i++){
-            if(B[i] <= A[aIdx]){
-                continue;
+        int bIdx = 0;
+        
+        for(int i=0; i<A.length; i++){
+            if(A[aIdx] >= B[bIdx]){
+                bIdx++;
             }
-            
-            answer++;
-            aIdx++;
+            else{
+                aIdx++;
+                bIdx++;
+                answer++;
+            }
         }
         
         return answer;
