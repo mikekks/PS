c7b9439ff4fedb050ea813d59ffbd736dcfcfba1
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2025-03-07 11:46:20 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 120 ms, Memory: 14912 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -12,23 +12,27 @@ class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
 		N = Integer.parseInt(s);
-
-		int[] num = {1, 2, 3};
-		int[] dp = new int[10005];
-		dp[0] = 1;
-
-		for(Integer cur : num){
-			for(int i=cur; i<=10000; i++){
-				dp[i] += dp[i - cur];
-			}
-		}
 		
+		int[][] dp = new int[10005][4];
+		dp[1][1] = 1;
+		dp[2][1] = 1;
+		dp[2][2] = 1;
+		dp[3][1] = 1;
+		dp[3][2] = 1;
+		dp[3][3] = 1;
+
+		for (int i = 4; i <= 10000; i++) {
+			dp[i][1] = dp[i - 1][1];
+			dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+			dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+		}
+
 		while (N > 0) {
 			N--;
 			s = br.readLine();
 			int target = Integer.parseInt(s);
 
-			System.out.println(dp[target]);
+			System.out.println(dp[target][1] + dp[target][2] + dp[target][3]);
 		}
 
 	}
