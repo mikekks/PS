6cc9a5577755ffc69fcdaee9ecedb7826cf9394b
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2025-03-21 21:04:09 +0900

    [Gold II] Title: 줄 세우기, Time: 492 ms, Memory: 112296 KB -BaekjoonHub

---
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	static int N, M, P;
+	static int[] index;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String st = br.readLine();
+
+		N = Integer.parseInt(st);
+
+		if(N == 1){
+			System.out.println(0);
+			return;
+		}
+
+		index = new int[N + 1];
+
+		st = br.readLine();
+		String[] split = st.split(" ");
+
+		int ans = 0;
+
+		for(int i=1; i<=N; i++){
+			int idx = Integer.parseInt(split[i - 1]);
+			index[idx] = index[idx - 1] + 1;
+		}
+
+		Arrays.sort(index);
+
+		System.out.println(N - index[N]);
+
+	}
+}
